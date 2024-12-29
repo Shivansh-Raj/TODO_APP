483db56664c3e9c3ba44b867f0d577ba1ef9fa43
Shivansh-Raj <shivanshrajdehl@gmail.com>
2024-12-30 00:34:54 +0530

    url updates

--- todo_back/back/urls.py
+++ todo_back/back/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from django.urls import path
 from .views import TasksAPI
 urlpatterns = [
-    path('task_set/<int:taskid>', TasksAPI.as_view(), name = "get Recommendations"),
+    path('task_set/<int:taskid>', TasksAPI.as_view(), name = "get Tasks"),
     # path('UserHistory/<int:show_id>', UserHistory.as_view(), name ="Adding to History"),
     # path('get_for_you', for_you, name ="For You"),
 ]
