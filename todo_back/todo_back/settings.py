9c13f2de77fb53bd231f29dcdabbee374d083414
Shivansh-Raj <shivanshrajdehl@gmail.com>
2024-12-29 20:20:01 +0530

    2nd Commit

--- todo_back/todo_back/settings.py
+++ todo_back/todo_back/settings.py
@@ -98,7 +98,11 @@ WSGI_APPLICATION = 'todo_back.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.getenv("DB_NAME"),
+        'USER':os.getenv("DB_USER"),
+        'PASSWORD':os.getenv("DB_PWD"),
+        'HOST':os.getenv("DB_HOST"),
+        'PORT':os.getenv("DB_POST"),
     }
 }
 
